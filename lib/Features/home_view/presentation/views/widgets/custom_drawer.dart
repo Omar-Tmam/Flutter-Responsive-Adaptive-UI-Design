178f57d3faae936cc5d65793efe1244640bdcab6
omar <omartmam9@gmail.com>
2025-08-02 02:39:36 +0300

    custom scroll view

--- lib/Features/home_view/presentation/views/widgets/custom_drawer.dart
+++ lib/Features/home_view/presentation/views/widgets/custom_drawer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/Core/utils/app_images.dart';
 import 'package:responsive_dashboard/Features/home_view/data/models/drawer_item_model.dart';
-import 'package:responsive_dashboard/Features/home_view/presentation/views/widgets/drawer_item.dart';
+import 'package:responsive_dashboard/Features/home_view/presentation/views/widgets/active_and_inactive_item.dart';
 import 'package:responsive_dashboard/Features/home_view/presentation/views/widgets/drawer_items_list_view.dart';
 import 'package:responsive_dashboard/Features/home_view/presentation/views/widgets/user_info_list_tile.dart';
 
@@ -12,18 +12,37 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Column(
-        children: [
-          UserInfoListTile(
-              image: Assets.imagesSmileface3,
-              title: 'Lekan Okeowo',
-              subtitle: 'demo@gmail.com'),
-          SizedBox(height: 8),
-          DrawerItemsListView()
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: UserInfoListTile(
+                image: Assets.imagesSmileface3,
+                title: 'Lekan Okeowo',
+                subtitle: 'demo@gmail.com'),
+          ),
+          SliverToBoxAdapter(child: SizedBox(height: 8)),
+          DrawerItemsListView(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(child: SizedBox()),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: 'Setting system', image: Assets.imagesSetting2),
+                ),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: 'Logout account', image: Assets.imagesLogout),
+                ),
+                SizedBox(
+                  height: 48,
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
   }
 }
-
-
--- lib/Features/home_view/presentation/views/widgets/drawer_item.dart
+++ lib/Features/home_view/presentation/views/widgets/drawer_item.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:responsive_dashboard/Core/utils/styles.dart';
 import 'package:responsive_dashboard/Features/home_view/data/models/drawer_item_model.dart';
 import 'package:responsive_dashboard/Features/home_view/presentation/views/widgets/active_and_inactive_item.dart';
 
