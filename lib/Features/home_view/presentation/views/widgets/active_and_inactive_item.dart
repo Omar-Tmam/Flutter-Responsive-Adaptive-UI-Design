213c14b180b37a1add3a188fefbdc73b8e6561fa
omar <omartmam9@gmail.com>
2025-08-01 10:44:09 +0300

    active and inactive item

---
+++ lib/Features/home_view/presentation/views/widgets/active_and_inactive_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:responsive_dashboard/Core/utils/styles.dart';
+import 'package:responsive_dashboard/Features/home_view/data/models/drawer_item_model.dart';
+
+class InActiveDrawerItem extends StatelessWidget {
+  const InActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItemModel.image),
+      title: Text(
+        drawerItemModel.title,
+        style: AppStyles.styleMedium16,
+      ),
+    );
+  }
+}
+
+class ActiveDrawerItem extends StatelessWidget {
+  const ActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItemModel.image),
+      title: Text(
+        drawerItemModel.title,
+        style: AppStyles.styleBold16,
+      ),
+      trailing: Container(
+        width: 3.267604351043701,
+  
+        decoration: BoxDecoration(
+          color: Color(0xff4eb7f2),
+        ),
+      ),
+    );
+  }
+}
--- lib/Features/home_view/presentation/views/widgets/drawer_item.dart
+++ lib/Features/home_view/presentation/views/widgets/drawer_item.dart
@@ -2,20 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:responsive_dashboard/Core/utils/styles.dart';
 import 'package:responsive_dashboard/Features/home_view/data/models/drawer_item_model.dart';
+import 'package:responsive_dashboard/Features/home_view/presentation/views/widgets/active_and_inactive_item.dart';
 
 class DrawerItem extends StatelessWidget {
-  const DrawerItem({super.key, required this.drawerItemModel, required this.isActive});
+  const DrawerItem(
+      {super.key, required this.drawerItemModel, required this.isActive});
   final DrawerItemModel drawerItemModel;
   final bool isActive;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(drawerItemModel.image),
-      title: Text(
-        drawerItemModel.title,
-        style: AppStyles.styleMedium16,
-      ),
-    );
+    return isActive
+        ? ActiveDrawerItem(drawerItemModel: drawerItemModel)
+        : InActiveDrawerItem(drawerItemModel: drawerItemModel);
   }
 }
+
