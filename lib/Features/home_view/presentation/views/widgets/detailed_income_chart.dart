fd8b91324d3d02cfc6d68517322bfdc4d31d08ce
omar <omartmam9@gmail.com>
2025-08-08 08:44:08 +0300

    create detailed income chart

---
+++ lib/Features/home_view/presentation/views/widgets/detailed_income_chart.dart
@@ -0,0 +1,56 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class DetailedIncomeChart extends StatefulWidget {
+  const DetailedIncomeChart({super.key});
+
+  @override
+  State<DetailedIncomeChart> createState() => _IncomeChartState();
+}
+
+class _IncomeChartState extends State<DetailedIncomeChart> {
+  int activeIndex = -1;
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(aspectRatio: 1, child: PieChart(getChartData()));
+  }
+
+  PieChartData getChartData() {
+    return PieChartData(
+        pieTouchData: PieTouchData(
+          enabled: true,
+          touchCallback: (p0, pieTouchResponse) {
+            activeIndex =
+                pieTouchResponse?.touchedSection?.touchedSectionIndex ?? -1;
+            setState(() {});
+          },
+        ),
+        sectionsSpace: 0,
+        sections: [
+          PieChartSectionData(
+            radius: activeIndex == 0 ? 50 : 40,
+            showTitle: false,
+            value: 40,
+            color: Color(0xff208CC8),
+          ),
+          PieChartSectionData(
+            radius: activeIndex == 1 ? 50 : 40,
+            showTitle: false,
+            value: 25,
+            color: Color(0xff4EB7F2),
+          ),
+          PieChartSectionData(
+            radius: activeIndex == 2 ? 50 : 40,
+            showTitle: false,
+            value: 20,
+            color: Color(0xff064061),
+          ),
+          PieChartSectionData(
+            radius: activeIndex == 3 ? 50 : 40,
+            showTitle: false,
+            value: 15,
+            color: Color(0xffE2DECD),
+          ),
+        ]);
+  }
+}
--- lib/Features/home_view/presentation/views/widgets/income_section_body.dart
+++ lib/Features/home_view/presentation/views/widgets/income_section_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/Core/utils/size_config.dart';
+import 'package:responsive_dashboard/Features/home_view/presentation/views/widgets/detailed_income_chart.dart';
 import 'package:responsive_dashboard/Features/home_view/presentation/views/widgets/income_chart.dart';
 import 'package:responsive_dashboard/Features/home_view/presentation/views/widgets/income_details.dart';
 
@@ -12,8 +13,9 @@ class IncomeSectionBody extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
     return width >= SizeConfig.desktop && width <= 1920
-        ? SizedBox()
+        ? Expanded(child: DetailedIncomeChart())
         : Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Expanded(
                 child: AspectRatio(
