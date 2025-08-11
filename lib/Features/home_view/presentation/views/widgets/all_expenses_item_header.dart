ce86191bd912a82bf14972ad2d1bdc533e4708b6
omar <omartmam9@gmail.com>
2025-08-11 18:34:09 +0300

    add constained box

--- lib/Features/home_view/presentation/views/widgets/all_expenses_item_header.dart
+++ lib/Features/home_view/presentation/views/widgets/all_expenses_item_header.dart
@@ -11,19 +11,27 @@ class AllExpensesItemHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Container(
-          width: 60,
-          height: 60,
-          decoration: ShapeDecoration(
-            color: imageBackground ?? const Color(0xFFFAFAFA),
-            shape: OvalBorder(),
+        Flexible(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 60),
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                width: 60,
+                height: 60,
+                decoration: ShapeDecoration(
+                  color: imageBackground ?? const Color(0xFFFAFAFA),
+                  shape: OvalBorder(),
+                ),
+                child: Center(
+                    child: SvgPicture.asset(
+                  image,
+                  colorFilter: ColorFilter.mode(
+                      imageColor ?? Color(0xff4EB7F2), BlendMode.srcIn),
+                )),
+              ),
+            ),
           ),
-          child: Center(
-              child: SvgPicture.asset(
-            image,
-            colorFilter: ColorFilter.mode(
-                imageColor ?? Color(0xff4EB7F2), BlendMode.srcIn),
-          )),
         ),
         Expanded(child: SizedBox()),
         Icon(
